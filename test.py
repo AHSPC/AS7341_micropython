0b6fdba89fcfdeeb1ef30c302924016e72d1328e
Bvngee Cord <nystromjp@gmail.com>
2024-06-06 13:54:57 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from machine import SoftI2C, Pin
 from as7341 import AS7341
 
 i2c = SoftI2C(sda=Pin(0), scl=Pin(1))
-print(i2c.scan())
 sensor = AS7341(i2c)
 
 sensor.led = True
@@ -12,6 +11,8 @@ for _ in range(100):
     readings = sensor.get_readings()
     # print(readings)
 
+    # Prints the color name and value for whatever the strongest wavelength is.
+    # NOTE: this is not really a very accurate way to tell which color the sensor is seeing.
     maxK = ""
     maxV = 0
     for k, v in readings.items():
